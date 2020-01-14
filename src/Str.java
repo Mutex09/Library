d01df012aa18e9759ec4165c893b72678afb2bc0
Mutex <47752425+Mutex09@users.noreply.github.com>
2020-01-14 23:40:54 +0300

    FirstAdd

---
+++ src/Str.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Str
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        //Search Book
+        BufferedReader messageFile;
+        String message = "";
+        StringBuilder messageB = new StringBuilder();
+        try {
+            messageFile = new BufferedReader(new FileReader("src\\Books.txt"));
+            while ((message=messageFile.readLine())!=null) {
+                messageB.append(message).append("\n");
+            }
+            messageFile.close();
+        } catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(messageB);
+        //Change/add description
+        String searchWord = "sas";
+        String changeWord = "This book is about adventure";
+
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src\\Books.txt")))) {
+            String strLine;
+            while ((strLine = br.readLine()) != null) {
+                if (!strLine.matches("(.)*\"" + searchWord + "\"(.)*"))
+                sb.append(strLine).append("\n");
+            }
+        }
+
+        try (FileWriter fileWriter = new FileWriter("src\\Books.txt")){
+
+            fileWriter.write(sb.toString());
+        }
+
+
+    }
+}
